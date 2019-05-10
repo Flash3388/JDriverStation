3a447b01e929e1622eae35a80476297eff062aea
Tom Tzook <tomtzook@gmail.com>
2019-05-11 02:19:36 +0300

    Working on native gradle defenitions

---
+++ src/dsjni/c/test.c
@@ -0,0 +1,6 @@
+#include <jni.h>
+
+JNIEXPORT void JNICALL
+Java_com_flash3388_frc_ds_Test_testC(JNIEnv *env, jobject obj) {
+    return;
+}
---
+++ src/main/java/com/flash3388/frc/ds/Test.java
@@ -0,0 +1,6 @@
+package com.flash3388.frc.ds;
+
+public class Test {
+
+    public native void testC();
+}
